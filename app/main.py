bb2e0d5d1d8f997051e1995570d61aca1051272b
iamsharduld <iamsharduld@gmail.com>
2020-07-16 01:21:29 +0530

    Added flask jwt

--- app/main.py
+++ app/main.py
@@ -6,10 +6,9 @@ import pandas as pd
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS, cross_origin
+from flask_jwt import JWT, jwt_required, current_identity
+from security import authenticate, identity
 
-app = Flask(__name__)
-api = Api(app)
-cors = CORS(app)
 
 googleSheetId = '1dPTIcVN9ux6mX5y8Xql_kvHZXm45mF9UXbdznytyoto'
 worksheetName = 'Code-J'
@@ -24,6 +23,15 @@ cols = cols[2:]
 X = data[data.columns]
 y = data['0Symptoms list']
 
+
+app = Flask(__name__)
+api = Api(app)
+cors = CORS(app)
+app.secret_key = 'jose'
+jwt = JWT(app, authenticate, identity)
+
+
+
 class Diagnosis(Resource):
 
     # parser = reqparse.RequestParser()
@@ -32,7 +40,7 @@ class Diagnosis(Resource):
     #     required=True,
     #     help="This field cannot be left blank"
     # )
-
+    @jwt_required()
     @cross_origin()
     def get(self):
         # data = Item.parser.parse_args()
@@ -51,7 +59,7 @@ class Diagnosis(Resource):
         
         return {'possibilities': list(diag)}
 
-    
+    @jwt_required()
     @cross_origin()
     def post(self):
         # print(request.get_json())
@@ -71,6 +79,7 @@ class Diagnosis(Resource):
 
 class SymptomList(Resource):
 
+    @jwt_required()
     @cross_origin()
     def get(self):
         return {'items': list(cols)}
